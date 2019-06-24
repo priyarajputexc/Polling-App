a735b95f74d27a09dd1947fdff59531b7e438d1a
priyarajputexc <priya@excellencetechnologies.info>
2019-06-24 09:58:10 +0530

    Login Page Completed

---
+++ src/app/components/login/login.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private http: HttpClient) { }
+
+  login(body) {
+    return this.http.get(`${environment.apiBase}/login?email=${body.email}&password=${body.password}`).toPromise();
+  }
+}
