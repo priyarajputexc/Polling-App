8da286cc36324715c3ab043e77d815c2660b4937
priyarajputexc <priya@excellencetechnologies.info>
2019-06-25 15:52:33 +0530

    Add Poll Component Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,20 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RegisterComponent } from './components/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CreateComponent } from './components/create/create.component';
+import { AuthComponent } from './components/auth/auth.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    DashboardComponent,
+    CreateComponent,
+    AuthComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
